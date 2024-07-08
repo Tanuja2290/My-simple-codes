30e59c9be82d523f29d5c7aad58b0cf2818507fc
Tanuja2290 <motetanuja@gmail.com>
2024-07-08 21:53:33 +0530

    Sum 1 to n without loop
    Java code to find sum of 1 to n numbers without using any loop

---
+++ Main.java
@@ -0,0 +1,14 @@
+﻿public class Main {
+  public static void main(String[] args) {
+    int n=23, mid=0, result=0;
+    mid=(n+1)/2;
+    if(n%2==0){
+     result=(n*mid)+mid;
+    }else{
+     result=n*mid;
+    }
+    System.out.println(result);
+   
+  }
+}
+
